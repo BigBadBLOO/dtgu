915ca8dcb35439c5155e9bb4aba9712cfa099417
Ефим <agapovefim@gmail.com>
2020-06-20 14:48:14 +0300

    bd

--- site_dtgu/wsgi.py
+++ site_dtgu/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/domains/ovz1.9134819807.pk7kn.vps.myjino.ru/dtgu')
+sys.path.append('/var/www/domains/ovz1.9134819807.pk7kn.vps.myjino.ru/env/lib/python3.6/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_dtgu.settings')
 
