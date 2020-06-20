1d79ac2851bcbb54d4fe5610699b11a553d11bf6
Ефим <agapovefim@gmail.com>
2020-06-20 19:13:20 +0300

    bd

--- app_dtgu/static/app_dtgu/js/index/index_prorector.js
+++ app_dtgu/static/app_dtgu/js/index/index_prorector.js
@@ -3,8 +3,8 @@ var specialty = [];
 var marks = [];
 var paramsMarks = [];
 
-// var type_id = -1;
 var specialitys_id = -1;
+
 $(document).ready(function () {
     ajax_message('/index_init/', {'csrfmiddlewaretoken': $($(tokenlist[0]).children()[0]).val()}, make_index)
 });
@@ -63,8 +63,8 @@ function make_result(data) {
 
     init_tree();
 
-    setTimeout(    makeGraph, 2000);
-    hide_load();
+    setTimeout(makeGraph, 2000);
+    setTimeout(hide_load, 1000);
 
 
 }
@@ -117,26 +117,48 @@ function getTree() {
         var paramMark_node = [];
         paramMark.forEach(function (param) {
             var children = search_list_in_json(paramsMarks,'HighOrderParamMark_id', param.id);
-
             var childrenParam = [];
-            children.forEach(function (childParam) {
-                var child_graph = [{
-                    text: '<div id="graph" style="display: table"></div>',
+
+            if(param.name === '% выпускников, трудоустроенных в ведущих компаниях, в течение года после выпуска'){
+                childrenParam.push(  {
+                    text: '<div id="graph_2_2_1" style="display: table"></div>',
                     type: 'childParamGraph',
-                    field_search: 'childParamGraph_' + childParam.id,
-                    pk: 'childParamGraph_' + childParam.id,
-                }];
+                    field_search: 'childParamGraph_' + param.id,
+                    pk: 'childParamGraph_' + param.id,
+                })
+            }
+
+            if(param.name === 'Отношение средней з/п выпускника в первый год работы к средней з/п по региону'){
+                childrenParam.push(  {
+                    text: '<div id="graph_2_2_2" style="display: table"></div>',
+                    type: 'childParamGraph',
+                    field_search: 'childParamGraph_' + param.id,
+                    pk: 'childParamGraph_' + param.id,
+                })
+            }
+
+            children.forEach(function (childParam) {
+                var child_graph = [];
+
+                if(childParam.name === 'Газпром'){
+                    child_graph.push(  {
+                        text: '<div id="graph" style="display: table"></div>',
+                        type: 'childParamGraph',
+                        field_search: 'childParamGraph_' + childParam.id,
+                        pk: 'childParamGraph_' + childParam.id,
+                    })
+                }
 
                 childrenParam.push({
-                    text: childParam.name,
+                    text: make_name(childParam.name),
                     type: 'childParam',
-                    nodes: childParam.name === 'Газпром' ? child_graph : [],
+                    nodes: child_graph,
                     field_search: 'childParam_' + childParam.id,
                     pk: childParam.id,
                 })
             });
             paramMark_node.push({
-                text: param.name + '<span class="pull-right" style="font-weight: 600">' + param.value + '</span>',
+                text: make_name(param.name) + '<span class="pull-right" style="font-weight: 600">' + param.value + '</span>',
                 type: 'param',
                 nodes:childrenParam,
                 field_search: 'param_' + param.id,
@@ -144,15 +166,34 @@ function getTree() {
             });
         });
         tree.push({
-            text: index + 1 + '. ' + item.name,
+            text: make_name(index + 1 + '. ' + item.name),
             type: 'mark',
             nodes: paramMark_node,
             field_search: 'mark_' + item.id,
             pk: item.id,
         });
     });
+    console.log(tree);
   return tree;
 }
+
+
+function expandChildrenNode(el,marker) {
+    let tree = $('#'+marker);
+    let nodeIdParent = $(el.parentNode).attr('data-nodeid');
+    let node = tree.treeview('getNodes', nodeIdParent);
+    if(node.state.expanded){
+        tree.treeview('collapseNode', [ [node], { silent: true, ignoreChildren: false } ]);
+    }else{
+        tree.treeview('expandNode', [ node, { levels: 1, silent: true } ]);
+    }
+}
+
+
+function make_name(name) {
+    return '<span onclick="expandChildrenNode(this,\'third_block\')">' + name + '</span>'
+}
+
 
 
 function _getChildren(node) {
@@ -164,37 +205,45 @@ function _getChildren(node) {
     return childrenNodes;
 }
 
+function make_card_graph(marker,text, column=12) {
+    return '<div class="col-sm-12 col-md-12 col-lg-' + column + '">\n' +
+        '                <div class="card">\n' +
+        '                    <div class="card-body">\n' +
+        '                        <p class="card-text pull-left">' + text + '</p>\n' +
+        '                        <canvas id="' + marker + '"></canvas>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>';
+}
 function makeGraph() {
+    var html = make_card_graph("LeftChart", "Динамика зарплаты", 6);
+    html += make_card_graph("RightChart", "Динамика вакансий", 6);
 
-    var html = '<div class="col-sm-12 col-md-12 col-lg-6">\n' +
-        '                <div class="card">\n' +
-        '                    <div class="card-body">\n' +
-        '                        <p class="card-text pull-left">Динамика зарплаты</p>\n' +
-        '                        <canvas id="LeftChart"></canvas>\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '            </div>';
-
-    html += '<div class="col-sm-12 col-md-12 col-lg-6">\n' +
-        '                <div class="card">\n' +
-        '                    <div class="card-body">\n' +
-        '                        <p class="card-text pull-left">Динамика вакансий</p>\n' +
-        '                        <canvas id="RightChart"></canvas>\n' +
-        '                    </div>\n' +
-        '                </div>\n' +
-        '            </div>';
-
+    var html_2_2_1 = make_card_graph("Chart_2_2_1", "% выпускников, трудоустроенных в ведущих компаниях, в течение 5 лет");
+    var html_2_2_2 = make_card_graph("Chart_2_2_2", "Отношение средней з/п выпускника за 5 лет");
+    
     $('#graph').html(html);
-    makeGraphData();
+    
+    $('#graph_2_2_1').html(html_2_2_1);
+    $('#graph_2_2_2').html(html_2_2_2);
+
+    try{
+        makeGraphData_1_2();
+    }catch (e) {
+        
+    }
+     
+
 }
 
-function makeGraphData() {
-
+function makeGraphData_1_2() {
     var data_for_left_chart = {};
     var data_for_right_chart = {};
 
-    var type_for_left_char = 'this_week';
-    var type_for_right_char = 'this_week';
+    var data_for_2_2_1_chart = {};
+    var data_for_2_2_2_chart = {};
+
+    var type_for_char = 'this_week';
 
     var options = {
         legend: {
@@ -233,10 +282,9 @@ function makeGraphData() {
                     display: false,
                 }
             }]
-
         }
-
     };
+
     Chart.defaults.global.hover.mode = 'index';
     Chart.defaults.global.elements.point.radius = 5;
     Chart.defaults.global.elements.point.hoverRadius = 7;
@@ -247,34 +295,66 @@ function makeGraphData() {
     Chart.defaults.scale.ticks.beginAtZero = true;
 
     Chart.defaults.global.elements.line.borderColor = '#0097cc';
-    Chart.defaults.global.elements.line.backgroundColor = 'rgba(146, 44, 136,.1)';
+    Chart.defaults.global.elements.line.backgroundColor = 'rgba(0,151,204,0.1)';
 
+    try {
+        var l_ctx = document.getElementById('LeftChart').getContext('2d');
+        var l_chart = new Chart(l_ctx, {
+            type: 'line',
+            options: options
+        });
 
-    var l_ctx = document.getElementById('LeftChart').getContext('2d');
-    var r_ctx = document.getElementById('RightChart').getContext('2d');
-    var l_chart = new Chart(l_ctx, {
-        type: 'line',
-        options: options
-    });
+        var r_ctx = document.getElementById('RightChart').getContext('2d');
+        var r_chart = new Chart(r_ctx, {
+            type: 'line',
+            options: options
+        });
+    }catch (e) {}
 
-    var r_chart = new Chart(r_ctx, {
-        type: 'line',
-        options: options
-    });
+    try {
+        var ctx_2_2_1 = document.getElementById('Chart_2_2_1').getContext('2d');
+        var chart_2_2_1 = new Chart(ctx_2_2_1, {
+            type: 'line',
+            options: options
+        });
+
+        var ctx_2_2_2 = document.getElementById('Chart_2_2_2').getContext('2d');
+        var chart_2_2_2 = new Chart(ctx_2_2_2, {
+            type: 'line',
+            options: options
+        });
+    }catch (e) {}
 
     function change_data_in_left_chart() {
-        if (type_for_left_char in data_for_left_chart) {
+        if (type_for_char in data_for_left_chart) {
             l_chart.clear();
-            l_chart.data = data_for_left_chart[type_for_left_char];
+            l_chart.data = data_for_left_chart[type_for_char];
             l_chart.update();
         }
     }
 
     function change_data_in_right_chart() {
-        if (type_for_right_char in data_for_right_chart) {
+        if (type_for_char in data_for_right_chart) {
             r_chart.clear();
-            r_chart.data = data_for_right_chart[type_for_right_char];
+            r_chart.data = data_for_right_chart[type_for_char];
             r_chart.update();
+        }
+    }
+
+    function change_data_in_2_2_1_chart() {
+        if (type_for_char in data_for_2_2_1_chart) {
+            chart_2_2_1.clear();
+            chart_2_2_1.data = data_for_2_2_1_chart[type_for_char];
+            chart_2_2_1.update();
+        }
+    }
+
+    function change_data_in_2_2_2_chart() {
+        console.log(data_for_2_2_2_chart);
+        if (type_for_char in data_for_2_2_2_chart) {
+            chart_2_2_2.clear();
+            chart_2_2_2.data = data_for_2_2_2_chart[type_for_char];
+            chart_2_2_2.update();
         }
     }
 
@@ -287,16 +367,23 @@ function makeGraphData() {
            data_for_right_chart = data.dataset.chart_leads_count;
             change_data_in_right_chart();
         }
+        if('chart_2_2_1' in  data.dataset){
+           data_for_2_2_1_chart = data.dataset.chart_2_2_1;
+            change_data_in_2_2_1_chart();
+        }
+        if('chart_2_2_2' in  data.dataset){
+           data_for_2_2_2_chart = data.dataset.chart_2_2_2;
+            change_data_in_2_2_2_chart();
+        }
     }
 
     var data = {
-
         dataset:{
             chart_profit_calc:{
                 this_week:{
                     datasets:[
                         {
-                            data:[78800, 75500,77900,79300,80000,79500]
+                            data:[64800, 76500,77900,79300,86000,89500]
                         }
                     ],
                     labels:['01.20','02.20','03.20','04.20','05.20','06.20']
@@ -311,11 +398,28 @@ function makeGraphData() {
                     ],
                     labels:['01.20','02.20','03.20','04.20','05.20','06.20']
                 }
-
-            }
+            },
+            chart_2_2_1:{
+                this_week:{
+                    datasets:[
+                        {
+                            data:[28, 40, 30, 34, 39, 42]
+                        }
+                    ],
+                    labels:['2015','2016','2017','2018','2019','2020']
+                }
+            },
+            chart_2_2_2:{
+                this_week:{
+                    datasets:[
+                        {
+                            data:[68, 79, 60, 65, 70, 75]
+                        }
+                    ],
+                    labels:['2015','2016','2017','2018','2019','2020']
+                }
+            },
         }
     };
     change_data_in_charts(data);
-
-
 }
